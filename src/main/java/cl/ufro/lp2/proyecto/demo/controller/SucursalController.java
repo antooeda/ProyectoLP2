1cf63c6bf46191f282cd8b71110858ed93309694
antooeda <48840207+antooeda@users.noreply.github.com>
2019-06-12 17:08:28 -0400

    12/06  17:08

--- src/main/java/cl/ufro/lp2/proyecto/demo/controller/SucursalController.java
+++ src/main/java/cl/ufro/lp2/proyecto/demo/controller/SucursalController.java
@@ -6,10 +6,14 @@
 package cl.ufro.lp2.proyecto.demo.controller;
 
 import cl.ufro.lp2.proyecto.demo.dao.SucursalDao;
+import cl.ufro.lp2.proyecto.demo.modelo.Sucursal;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,7 +29,29 @@ public class SucursalController {
     
     @GetMapping("/crearSucursal")
     public String crearSucursal(Model model){
-        model.addAttribute("dato", "temuco");
+        model.addAttribute("dato", "ddfggf");
+        model.addAttribute("sucursal", new Sucursal());
         return "crearSucursal";
+    }
+    
+     @PostMapping("/crearSucurusalForm")
+    public String obtenerSucursal(@ModelAttribute Sucursal sucursal){
+        
+        //System.out.println(sucursal.getNombre());
+        
+        sDAO.save(sucursal);
+        return "index";
+    }
+    
+    
+    @GetMapping("/obtenerSucursales")
+    public String getSucursalesView(Model model){
+        
+        List<Sucursal> sucursales = sDAO.findAll();
+        
+        model.addAttribute("sucursales", sucursales);
+        
+        
+        return "sucursales";
     }
 }
