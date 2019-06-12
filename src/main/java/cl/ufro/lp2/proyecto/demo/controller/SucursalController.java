1023ac36537a3e4dd242bfb21d4c6debd16365aa
antooeda <48840207+antooeda@users.noreply.github.com>
2019-06-12 16:13:08 -0400

    12/06  16:13

--- src/main/java/cl/ufro/lp2/proyecto/demo/controller/SucursalController.java
+++ src/main/java/cl/ufro/lp2/proyecto/demo/controller/SucursalController.java
@@ -5,8 +5,11 @@
  */
 package cl.ufro.lp2.proyecto.demo.controller;
 
+import cl.ufro.lp2.proyecto.demo.dao.SucursalDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,6 +20,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SucursalController {
   
     
+    @Autowired
+    private SucursalDao sDAO;
     
-    
+    @GetMapping("/crearSucursal")
+    public String crearSucursal(Model model){
+        model.addAttribute("dato", "temuco");
+        return "crearSucursal";
+    }
 }
